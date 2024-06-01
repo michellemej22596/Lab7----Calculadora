0534513ce04146898ca0767d9256ff11e60b3f57
michellemej22596 <mej22596@uvg.edu.gt>
2024-06-01 01:25:00 -0600

    Last commit

--- calculadora/next.config.mjs
+++ calculadora/next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+const nextConfig = {
+    output: 'export',
+  };
+  
+  export default nextConfig;
+  
